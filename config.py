f20c98bc7f0541ef650fb253dbc05ee5321ecb68
Isaac Rehg <isaacrehg@gmail.com>
2023-11-21 14:38:42 -0600

    add scissorhands with llama 7b

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 import argparse
-from typing import List
+from typing import List, Tuple
 
 @dataclass
 class Config:
@@ -8,18 +8,26 @@ class Config:
     effective_batch_size: int = 64
     lr: float = 1e-3
     lrd_gamma: float = 0.1
-    lrd_steps: List[int] = field(default_factory=[5000, 14000])
+    lrd_steps: List[int] = field(default_factory=lambda: [5000, 14000])
     eval_interval: int = 50
     save_interval: int = 50
     wandb_api_key: str = None
     experiment_id: str = None
     do_consolidate: bool = True
+    consolidate_method: str = "scissorhands"
+    control_layers: List[int] = field(default_factory=lambda: [])
     context_length: int = 400
     consolidate_length: int = 200
     train_length: int = 200
+    consolidate_range: List[int] = field(default_factory=lambda: [0, 200])
+    fit_range: List[int] = field(default_factory=lambda: [200, 400])
     consolidate_ratio: float = 0.5
+    min_fit_offset: int = 0
     device: str = "cuda:0"
     scale_v: bool = False
+
+    def do_modify(self) -> bool:
+        return self.do_consolidate
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--batch-size", default=8, type=int)
@@ -30,8 +38,16 @@ parser.add_argument("--lrd-steps", nargs="*", default=[5000, 14000], type=int)
 parser.add_argument("--eval-interval", default=50, type=int)
 parser.add_argument("--save-interval", default=50, type=int)
 parser.add_argument("--wandb-api-key", default=None, type=str)
-parser.add_argument("--experiment-id", default="QKNorm")
+parser.add_argument("--experiment-id", default="scissorhands")
 parser.add_argument("--scale-v", action="store_true")
+parser.add_argument("--consolidate-method", default="scissorhands", choices=["scissorhands"])
+parser.add_argument("--control-layers", nargs="*", type=int, help="indices of layers to skip cache consolidation")
+parser.add_argument("--do-consolidate", action="store_true")
+parser.add_argument("--consolidate-range", nargs=2, default=[0, 200], help="range of keys to consolidate")
+parser.add_argument("--consolidate-ratio", default=0.5, help="compression rate for range of keys being consolidated")
+parser.add_argument("--fit-range", nargs=2, default=[200, 400], help="range of queries used to fit consolidation")
+parser.add_argument("--min-fit-offset", default=0, help="minimum distance from consolidated keys to first query used to determine consolidation")
+
 args, _ = parser.parse_known_args()
 
 CONFIG = Config(
@@ -44,5 +60,11 @@ CONFIG = Config(
     save_interval=args.save_interval,
     wandb_api_key=args.wandb_api_key,
     experiment_id=args.experiment_id,
-    scale_v=args.scale_v
+    do_consolidate=args.do_consolidate,
+    consolidate_method=args.consolidate_method,
+    control_layers=args.control_layers,
+    consolidate_ratio=args.consolidate_ratio,
+    consolidate_range=args.consolidate_range,
+    fit_range=args.fit_range,
+    min_fit_offset=args.min_fit_offset,
 )
---
+++ llama_drop_in.py
@@ -0,0 +1,228 @@
+from transformers.models.llama.modeling_llama import LlamaAttention
+from config import CONFIG
+from typing import Tuple, Optional
+import types
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import warnings
+import math
+
+
+class LayerCounter:
+    def __init__(self, n_layer: int = 32) -> None:
+        self.n_layer = n_layer
+        self.ctr = 0
+
+    def increment(self):
+        self.ctr = (self.ctr + 1) % self.n_layer
+    
+    def is_control(self):
+        return self.ctr in CONFIG.control_layers
+
+
+class Globals:
+    def __init__(self, n_layer: int = 32) -> None:
+        self.layer_ctr = LayerCounter(n_layer=n_layer)
+        self.vars = {}
+
+    
+"""Apply scissorhands (https://arxiv.org/pdf/2305.17118.pdf) to prune keys exhibiting low attention"""
+def consolidate_scissorhands(attn_weights: torch.Tensor):
+    c_range = CONFIG.consolidate_range
+    f_range = CONFIG.fit_range
+
+    consolidate_attn = attn_weights[...,f_range,c_range]
+    max_activations: torch.Tensor = consolidate_attn.cummax(dim=-2).values
+
+    thresholds = torch.quantile(max_activations, CONFIG.consolidate_ratio, dim=-1)
+    evict_mask = max_activations < thresholds
+    if (evict_mask.sum(dim=-1) != int(consolidate_attn.shape[-1] * CONFIG.consolidate_ratio)).any():
+        GLOBALS.vars['failing_attn'] = attn_weights.cpu()
+        GLOBALS.vars['failing_mask'] = evict_mask.cpu()
+        GLOBALS.vars['failing_thresholds'] = thresholds.cpu()
+        raise AssertionError
+
+    attn_weights[...,f_range,c_range][evict_mask] = 0
+
+    return attn_weights
+
+
+def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
+    """
+    This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
+    num_key_value_heads, seqlen, head_dim) to (batch, num_attention_heads, seqlen, head_dim)
+    """
+    batch, num_key_value_heads, slen, head_dim = hidden_states.shape
+    if n_rep == 1:
+        return hidden_states
+    hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
+    return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
+
+
+def apply_rotary_pos_emb(q, k, cos, sin, position_ids, unsqueeze_dim=1):
+    """Applies Rotary Position Embedding to the query and key tensors.
+
+    Args:
+        q (`torch.Tensor`): The query tensor.
+        k (`torch.Tensor`): The key tensor.
+        cos (`torch.Tensor`): The cosine part of the rotary embedding.
+        sin (`torch.Tensor`): The sine part of the rotary embedding.
+        position_ids (`torch.Tensor`):
+            The position indices of the tokens corresponding to the query and key tensors. For example, this can be
+            used to pass offsetted position ids when working with a KV-cache.
+        unsqueeze_dim (`int`, *optional*, defaults to 1):
+            The 'unsqueeze_dim' argument specifies the dimension along which to unsqueeze cos[position_ids] and
+            sin[position_ids] so that they can be properly broadcasted to the dimensions of q and k. For example, note
+            that cos[position_ids] and sin[position_ids] have the shape [batch_size, seq_len, head_dim]. Then, if q and
+            k have the shape [batch_size, heads, seq_len, head_dim], then setting unsqueeze_dim=1 makes
+            cos[position_ids] and sin[position_ids] broadcastable to the shapes of q and k. Similarly, if q and k have
+            the shape [batch_size, seq_len, heads, head_dim], then set unsqueeze_dim=2.
+    Returns:
+        `tuple(torch.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
+    """
+    cos = cos[position_ids].unsqueeze(unsqueeze_dim)
+    sin = sin[position_ids].unsqueeze(unsqueeze_dim)
+    q_embed = (q * cos) + (rotate_half(q) * sin)
+    k_embed = (k * cos) + (rotate_half(k) * sin)
+    return q_embed, k_embed
+
+
+def rotate_half(x):
+    """Rotates half the hidden dims of the input."""
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2 :]
+    return torch.cat((-x2, x1), dim=-1)
+
+
+def modify_llama_attention(model):
+    for name, module in reversed(model._modules.items()):
+        if len(list(module.children())) > 0:
+            modify_llama_attention(
+                module,
+            )
+
+        if isinstance(module, LlamaAttention):
+            model._modules[name].forward = types.MethodType(
+                modified_llama_attn_forward, model._modules[name]
+            )
+
+
+def modified_llama_attn_forward(
+    self,
+    hidden_states: torch.Tensor,
+    attention_mask: Optional[torch.Tensor] = None,
+    position_ids: Optional[torch.LongTensor] = None,
+    past_key_value: Optional[Tuple[torch.Tensor]] = None,
+    output_attentions: bool = False,
+    use_cache: bool = False,
+    **kwargs,
+) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+    if "padding_mask" in kwargs:
+        warnings.warn(
+            "Passing `padding_mask` is deprecated and will be removed in v4.37. Please make sure use `attention_mask` instead.`"
+        )
+
+    bsz, q_len, _ = hidden_states.size()
+
+    if self.config.pretraining_tp > 1:
+        key_value_slicing = (self.num_key_value_heads * self.head_dim) // self.config.pretraining_tp
+        query_slices = self.q_proj.weight.split(
+            (self.num_heads * self.head_dim) // self.config.pretraining_tp, dim=0
+        )
+        key_slices = self.k_proj.weight.split(key_value_slicing, dim=0)
+        value_slices = self.v_proj.weight.split(key_value_slicing, dim=0)
+
+        query_states = [F.linear(hidden_states, query_slices[i]) for i in range(self.config.pretraining_tp)]
+        query_states = torch.cat(query_states, dim=-1)
+
+        key_states = [F.linear(hidden_states, key_slices[i]) for i in range(self.config.pretraining_tp)]
+        key_states = torch.cat(key_states, dim=-1)
+
+        value_states = [F.linear(hidden_states, value_slices[i]) for i in range(self.config.pretraining_tp)]
+        value_states = torch.cat(value_states, dim=-1)
+
+    else:
+        query_states = self.q_proj(hidden_states)
+        key_states = self.k_proj(hidden_states)
+        value_states = self.v_proj(hidden_states)
+
+    query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+    key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+    value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+
+    kv_seq_len = key_states.shape[-2]
+    if past_key_value is not None:
+        kv_seq_len += past_key_value[0].shape[-2]
+    cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
+    query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+
+    if past_key_value is not None:
+        # reuse k, v, self_attention
+        key_states = torch.cat([past_key_value[0], key_states], dim=2)
+        value_states = torch.cat([past_key_value[1], value_states], dim=2)
+
+    past_key_value = (key_states, value_states) if use_cache else None
+
+    key_states = repeat_kv(key_states, self.num_key_value_groups)
+    value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+    attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+
+    if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
+        raise ValueError(
+            f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
+            f" {attn_weights.size()}"
+        )
+
+    if attention_mask is not None:
+        if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+            raise ValueError(
+                f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+            )
+        attn_weights = attn_weights + attention_mask
+
+    # upcast attention to fp32
+    attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+    attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
+    
+    ### Apply scissorhands (https://arxiv.org/pdf/2305.17118.pdf) to prune keys exhibiting low attention
+    ### Only apply to keys generated during this forward pass
+    if (
+        CONFIG.do_consolidate and
+        CONFIG.consolidate_method == "scissorhands" and
+        not GLOBALS.layer_ctr.is_control()
+    ):
+        attn_weights = consolidate_scissorhands(attn_weights)
+    ###
+    
+    attn_output = torch.matmul(attn_weights, value_states)
+
+    if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+        raise ValueError(
+            f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+            f" {attn_output.size()}"
+        )
+
+    attn_output = attn_output.transpose(1, 2).contiguous()
+
+    attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
+
+    if self.config.pretraining_tp > 1:
+        attn_output = attn_output.split(self.hidden_size // self.config.pretraining_tp, dim=2)
+        o_proj_slices = self.o_proj.weight.split(self.hidden_size // self.config.pretraining_tp, dim=1)
+        attn_output = sum([F.linear(attn_output[i], o_proj_slices[i]) for i in range(self.config.pretraining_tp)])
+    else:
+        attn_output = self.o_proj(attn_output)
+
+    if not output_attentions:
+        attn_weights = None
+
+    ### Increment layer
+    GLOBALS.layer_ctr.increment()
+    ###
+
+    return attn_output, attn_weights, past_key_value
+
+
+GLOBALS = Globals()
